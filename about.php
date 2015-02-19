62e16d7e744be9aea1e10e6f8ebbbad2abe7b711
Zach Johnson <me@zajohnson.com>
2015-02-18 16:07:50 -0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * about.php
+ */
+ 
+require_once "includes/config.php";
+
+include "theme/header.php";
+?>
+ 
+<h1>About</h1>
+<p>Your lorem ipsum text here.</p>
+ 
+<?php
+include "theme/footer.php";
